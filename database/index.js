14288c86d20a6f4c41449f0be044f51b0b210e8d
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-15 12:09:54 -0700

    feat: add database schema and node to package dependencies.

--- database/index.js
+++ database/index.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner_id: Number,
+  owner_login: String,
+  stargazers_count: Number,
+  watchers_count: Number,
+  forks_count: Number,
+  total_count: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
