12d6b35f326c87408fff12dc94c826894960e728
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-15 16:55:09 -0700

    feat: add getReposByUsername function to helpers directory.

--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,30 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `/users/${username}/repos`,
+    method: 'get',
     headers: {
       'User-Agent': 'request',
+      'accept': 'application/vnd.github+json',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+
+  // return a promise from axios get request
+  return axios(options);
+    // .then((response) => {
+
+    // })
+    // .catch((error) => {
+
+    // });
 
 }
 
